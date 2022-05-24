8363fe58c398ee83e25dcdb34aa52f50171a77ea
ice949 <justineimasiku21@gmail.com>
2022-05-24 10:23:01 +0200

    Modify btn functionalities and fix linter erors

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 function createTemplate(books) {
-  let main = document.querySelector(".books-container");
+  const main = document.querySelector('.books-container');
 
   books.forEach((item, index) => {
-    let bookTemplate = document.querySelector(".book-template");
-    let book = bookTemplate.content.firstElementChild.cloneNode(true);
+    const bookTemplate = document.querySelector('.book-template');
+    const book = bookTemplate.content.firstElementChild.cloneNode(true);
 
-    let bookTitle = book.querySelector(".book-title");
+    const bookTitle = book.querySelector('.book-title');
     bookTitle.textContent = item.title;
 
-    let bookAuthor = book.querySelector(".book-author");
+    const bookAuthor = book.querySelector('.book-author');
     bookAuthor.textContent = item.author;
 
-    let removeBookButton = book.querySelector(".remove-book");
-    removeBookButton.addEventListener("click", (e) => {
+    const removeBookButton = book.querySelector('.remove-book');
+    removeBookButton.addEventListener('click', (e) => {
       e.preventDefault();
-      const books = JSON.parse(localStorage.getItem("books"));
+      const books = JSON.parse(localStorage.getItem('books'));
       books.splice(index, 1);
 
-      localStorage.setItem("books", JSON.stringify(books));
+      localStorage.setItem('books', JSON.stringify(books));
       book.parentNode.removeChild(book);
     });
 
@@ -26,26 +26,26 @@ function createTemplate(books) {
 }
 
 function checkFormObject() {
-  if (!localStorage.getItem("books")) {
+  if (!localStorage.getItem('books')) {
     // if not, create an empty object
     const books = [];
     // serialize the object to local storage
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   } else {
-    const books = JSON.parse(localStorage.getItem("books"));
+    const books = JSON.parse(localStorage.getItem('books'));
     createTemplate(books);
   }
 }
 
 window.onload = () => {
   checkFormObject();
-  const addBookForm = document.querySelector("#addBookForm");
+  const addBookForm = document.querySelector('#addBookForm');
   addBookForm.onsubmit = (e) => {
     const bookItem = Object.fromEntries(new FormData(e.target).entries());
-    const books = JSON.parse(localStorage.getItem("books"));
+    const books = JSON.parse(localStorage.getItem('books'));
     books.push(bookItem);
 
     // serialize form object to local storage
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   };
 };
